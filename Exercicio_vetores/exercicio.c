8e5ca99fea35fc8a5d905d8140d4b58884e386bf
João Pedro <joaopedrocalacavasconcelos@gmail.com>
2026-08-26 20:13:49 +0000

    Simplicado

--- Exercicio_vetores/exercicio.c
+++ Exercicio_vetores/exercicio.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 
 int main() {
+
     float nota1[30], nota2[30], media[30];
     float mediaTurma = 0;
 
     for (int i = 0; i < 30; i++) {
-        printf("Digite a primeira nota do aluno %d: ", i + 1);
+
+        printf("Nota 1 do aluno %d: ", i + 1);
         scanf("%f", &nota1[i]);
 
-        printf("Digite a segunda nota do aluno %d: ", i + 1);
+        printf("Nota 2 do aluno %d: ", i + 1);
         scanf("%f", &nota2[i]);
 
         media[i] = (nota1[i] * 2 + nota2[i] * 3) / 5;
 
-        mediaTurma += media[i];
+        mediaTurma = mediaTurma + media[i];
     }
 
     mediaTurma = mediaTurma / 30;
 
-    printf("\n==============================\n");
-    printf("Media da turma: %.2f\n", mediaTurma);
-    printf("==============================\n");
+    printf("\nMedia da turma: %.2f\n", mediaTurma);
 
     printf("\nAlunos acima da media da turma:\n");
 
     for (int i = 0; i < 30; i++) {
+
         if (media[i] > mediaTurma) {
             printf("\nAluno %d\n", i + 1);
-            printf("Primeira nota: %.2f\n", nota1[i]);
-            printf("Segunda nota: %.2f\n", nota2[i]);
+            printf("Nota 1: %.2f\n", nota1[i]);
+            printf("Nota 2: %.2f\n", nota2[i]);
             printf("Media: %.2f\n", media[i]);
         }
     }
